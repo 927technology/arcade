01adcfcc011a35dcb11d6522eac70525a3ca9540
Chris Murray <chris.m.murray@oracle.com>
2022-03-22 12:13:16 -0500

    deleted start.sh, replaced with archade.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-platform=${1}
-game=${2}
-
-docker run -it --rm -v /dev/snd:/dev/snd --privileged --name mame --hostname mame -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v ${HOME}:${HOME} -v /etc/passwd:/etc/passwd -v ~/games/mame:/home/mame/games 927technology/mame:0.1 /usr/games/mame ${platform} -cart "/home/mame/games/${platform}/${game}"
