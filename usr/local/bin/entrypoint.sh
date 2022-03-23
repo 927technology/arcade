d000118286ad4d501c7613f94664964586958c8a
Chris Murray <cmurray@927.technology>
2022-03-22 20:41:28 -0500

    updated arcade.sh and entrypoint.sh

--- usr/local/bin/entrypoint.sh
+++ usr/local/bin/entrypoint.sh
@@ -3,10 +3,7 @@
 case ${PLATFORM} in 
     nes)
         platform_bin=/usr/games/mame
-        platform_path=~/games/mame/nes
-        platform_cmd="${platform_bin} ${PLATFORM} -cart ${platform_path}/${ROM}"
+        platform_path=/home/arcade/games/mame/nes
+        "${platform_bin}" ${PLATFORM} -cart "${platform_path}/${ROM}"
     ;;
 esac
-
-#run platform_cmd if path and bin are present
-[ -d ${platform_path} ] & [ -f ${platform_bin} ] && ${platform_cmd}
