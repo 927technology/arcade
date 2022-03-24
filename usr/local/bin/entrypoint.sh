32d78636be155f1f96f3a4bcf763207f6a2d2233
Chris Murray <cmurray@927.technology>
2022-03-24 12:33:57 -0500

    added support for cps1-3, neogeo, fixed a7800 in entrypoint.sh

--- usr/local/bin/entrypoint.sh
+++ usr/local/bin/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 case ${PLATFORM} in 
-    a2600 | a5200 | a7800 | nes | snes)
+    a2600 | a5200 | nes | snes)
         platform_bin=/usr/games/mame
         platform_path=/home/arcade/games/mame/${PLATFORM}
         "${platform_bin}" ${PLATFORM} -cart "${platform_path}/${ROM}"
+    ;;
+    a7800 | cps | cps2 | cps3 | neo)
+        platform_bin=/usr/games/mame
+        platform_path=/home/arcade/games/mame/${PLATFORM}
+        "${platform_bin}" -rp ${platform_path} "${ROM}"
     ;;
 esac
